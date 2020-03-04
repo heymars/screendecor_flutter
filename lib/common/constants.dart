73cab8797630ada813d6e0fe0490edca161ae137
sujeet <skvishal875@gmail.com>
2020-03-04 11:42:13 +0530

    Few constants removed

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,10 +1,8 @@
 class Constants {
 //  static const String S3_BUCKET_URL =
 //      'https://gohive.s3.ap-south-1.amazonaws.com/';
-  static const String HOST = 'api.wittyvows.appstage.co';
+  static const String HOST = 'https://api.unsplash.com/';
   static const String BASE_API_ENDPOINT = '/api';
-  static const String BASE_API_URL = 'http://api.wittyvows.appstage.co/api';
+  static const String BASE_API_URL = 'https://api.unsplash.com/';
   static const String RUPEE_SYMBOL = '₹';
-  static const String GOOGLE_MAPS_API_KEY =
-      "AIzaSyDZ73td4H1Cbdb0saKGIx4T098lSsfAc2w";
 }
