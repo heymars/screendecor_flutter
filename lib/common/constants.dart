b2eebc82e1af428edeecb58a4c0374c8c3562ff8
sujeet <skvishal875@gmail.com>
2020-03-04 11:43:37 +0530

    Few comments removed

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,6 +1,4 @@
 class Constants {
-//  static const String S3_BUCKET_URL =
-//      'https://gohive.s3.ap-south-1.amazonaws.com/';
   static const String HOST = 'https://api.unsplash.com/';
   static const String BASE_API_ENDPOINT = '/api';
   static const String BASE_API_URL = 'https://api.unsplash.com/';
